e15969f6ee6d26e57ab3fdcb540d006a53d345fb
dominiksimgen <simgen.d@googlemail.com>
2021-12-05 00:15:34 +0100

    user story 33 account login umgesetzt

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request, Blueprint,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 import requests, os
-from . forms import LoginForm, RegisterForm
+from . forms import LoginForm, RegisterForm, ChangePasswordForm
 from . import db
 from .models import User
 
@@ -38,8 +38,13 @@ def register():
     if register_form.validate_on_submit():
         if User.query.filter_by(email=request.form.get('email')).first():
             #Benutzer existiert bereits
-            flash("Account with this email adress already exists. Try login instead.")
+            flash("Es liegt bereits eine Registrierung mit dieser Email vor. Bitte Login probieren.")
             return redirect(url_for('auth.login'))
+        
+        elif register_form.password.data != register_form.password_repeated.data:
+            flash('Neue Passwörter stimmen nicht überein!')
+            return redirect(url_for('auth.register'))
+        
         new_user=User(
             email = register_form.email.data,
             password=generate_password_hash(
@@ -47,15 +52,40 @@ def register():
                 method='pbkdf2:sha256', 
                 salt_length=8
             ),
-            name = register_form.name.data
+            role = register_form.role.data
         )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        return redirect(url_for('views.timeline'))
+        return redirect(url_for('views.home'))
 
     return render_template("register.html", form=register_form)
 
+
+@auth.route('/changepw', methods=['POST', 'GET'])
+@login_required
+def changepw():
+    changepw_form = ChangePasswordForm()
+    if changepw_form.validate_on_submit():
+        user = current_user
+        if check_password_hash(user.password, changepw_form.old_pw.data) == False:
+            flash('Altes Passwort nicht korrekt!')
+            return redirect(url_for('auth.changepw'))
+
+        elif changepw_form.new_pw.data != changepw_form.new_pw_repeated.data:
+            flash('Neue Passwörter stimmen nicht überein!')
+            return redirect(url_for('auth.changepw'))
+
+        elif check_password_hash(user.password, changepw_form.old_pw.data) == True:
+            current_user.password = generate_password_hash(
+                changepw_form.new_pw.data,
+                method='pbkdf2:sha256', 
+                salt_length=8
+            )
+            db.session.commit()
+            return redirect(url_for('views.home'))
+    
+    return render_template("changepw.html", form=changepw_form)
 
 
 @auth.route('/logout')
--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, TextAreaField
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, URL, Optional
 
 
@@ -8,11 +8,18 @@ from wtforms.validators import DataRequired, URL, Optional
 class LoginForm(FlaskForm):
     email = StringField(label="Email", validators=[DataRequired()])
     password = PasswordField(label="Password", validators=[DataRequired()])
-    submit = SubmitField("Let me in!")
+    submit = SubmitField("Login")
 
 
 class RegisterForm(FlaskForm):
     email = StringField(label="Email", validators=[DataRequired()])
-    password = StringField(label="Password", validators=[DataRequired()])
-    name = StringField(label="Username", validators=[DataRequired()])
-    submit = SubmitField("Sign me up!")
+    password = PasswordField(label="Passwort", validators=[DataRequired()])
+    password_repeated = PasswordField(label="Passwort wiederholen", validators=[DataRequired()])
+    role = SelectField(label="Kunde oder Dienstleister?", validators=[DataRequired()], choices=["Kunde", "Dienstleister"])
+    submit = SubmitField("Registrierung absenden")
+
+class ChangePasswordForm(FlaskForm):
+    old_pw = PasswordField(label="Altes Passwort", validators=[DataRequired()])
+    new_pw = PasswordField(label="Neues Passwort", validators=[DataRequired()])
+    new_pw_repeated = PasswordField(label="Neues Passwort wiederholen", validators=[DataRequired()])
+    submit = SubmitField("Passwortänderung übernehmen")
