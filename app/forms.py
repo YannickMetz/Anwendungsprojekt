966a5039d41be0b4338c35a7332dad06b9c863fe
dominiksimgen <simgen.d@googlemail.com>
2021-12-18 00:38:29 +0100

    anmeldeformulare kunde und dienstleister

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, TextAreaField, SelectField
+from wtforms.fields.core import DateField, IntegerField
 from wtforms.validators import DataRequired, URL, Optional
 
 
@@ -12,11 +13,36 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
+    role = SelectField(label="Kunde oder Dienstleister?", validators=[DataRequired()], choices=["Kunde", "Dienstleister"])
+    submit = SubmitField("Registrierung fortsetzen")
+
+
+class RegisterCustomerForm(FlaskForm):
+    k_vorname = StringField(label="Vorname", validators=[DataRequired()])
+    k_nachname = StringField(label="Nachname", validators=[DataRequired()])
+    k_geburtstatum = DateField(label="Geburtsdatum (TT.MM.YYYY)", validators=[DataRequired()], format='%d.%m.%Y')
+    k_straße = StringField(label="Straße und Hausnummer", validators=[DataRequired()])
+    k_plz = StringField(label="Postleitzahl", validators=[DataRequired()])
+    k_ort = StringField(label="Wohnort", validators=[DataRequired()])
     email = StringField(label="Email", validators=[DataRequired()])
     password = PasswordField(label="Passwort", validators=[DataRequired()])
     password_repeated = PasswordField(label="Passwort wiederholen", validators=[DataRequired()])
-    role = SelectField(label="Kunde oder Dienstleister?", validators=[DataRequired()], choices=["Kunde", "Dienstleister"])
-    submit = SubmitField("Registrierung absenden")
+    submit = SubmitField("Registrierung abschließen")
+
+
+class RegisterBusinessForm(FlaskForm):
+    d_vorname = StringField(label="Vorname", validators=[DataRequired()])
+    d_nachname = StringField(label="Nachname", validators=[DataRequired()]) 
+    firmenname = StringField(label="Firmenname", validators=[DataRequired()])
+    d_geburtstatum = DateField(label="Geburtsdatum (TT.MM.YYYY)", validators=[DataRequired()], format='%d.%m.%Y')
+    d_straße = StringField(label="Straße und Hausnummer", validators=[DataRequired()])
+    d_plz = StringField(label="Postleitzahl", validators=[DataRequired()])
+    d_ort = StringField(label="Wohnort", validators=[DataRequired()])
+    radius = IntegerField(label="Radius in dem sie Dienstleistungen anbieten möchten", validators=[DataRequired()])
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Passwort", validators=[DataRequired()])
+    password_repeated = PasswordField(label="Passwort wiederholen", validators=[DataRequired()])
+    submit = SubmitField("Registrierung abschließen")
 
 class ChangePasswordForm(FlaskForm):
     old_pw = PasswordField(label="Altes Passwort", validators=[DataRequired()])
