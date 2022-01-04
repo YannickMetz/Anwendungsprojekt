346914d221951e678a15c12280d83ef0fb22cbb1
Skrinto <97043243+Skrinto@users.noreply.github.com>
2022-01-04 16:43:55 +0100

    Zeile auskommentiert

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, PasswordField, TextAreaField, Sele
 from wtforms import DateField, IntegerField
 from wtforms.validators import DataRequired, URL, Optional
 from flask_wtf.file import FileAllowed
-from wtforms.fields.html5 import DateField
+#from wtforms.fields.html5 import DateField
 from flask_ckeditor import CKEditorField
 
 class LoginForm(FlaskForm):
