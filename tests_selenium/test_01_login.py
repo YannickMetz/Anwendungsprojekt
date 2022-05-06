1a7733576faf5a78ed8753fd26f120c525da34c0
Yannick <ymetz@hotmail.de>
2022-05-06 15:10:48 +0200

    selenium import

---
+++ tests_selenium/test_01_login.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+import unittest
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome("G:/SynologyDrive/Uni/7. Semester/Web-Technologie/Git-Repo/tests_selenium/chromedriver.exe")
