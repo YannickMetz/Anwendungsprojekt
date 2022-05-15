64b5828520a8d6f1c46c031f18f30aea97d3dfd7
dominiksimgen <simgen.d@googlemail.com>
2022-05-15 23:15:46 +0200

    Test Skripte angepasst

--- coverage.bash
+++ coverage.bash
@@ -1,2 +1,5 @@
 #! /bin/bash
-python3 -m pytest tests_pytest/ --cov=app
+python3 -m pytest tests_pytest/ --cov-report html --cov-report xml:cov.xml  --cov=app
+
+# HTML files in "htmlcov/" lassen sich mit "Live Preview" VS Code Extension anzeigen (Rechtsclick)
+# cov.xml wird verwendet von "Coverage Gutters" VS Code Extension für Preview im Quelltext 
