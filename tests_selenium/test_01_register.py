9fd6f50d8915ad6482db49a03493d20878809d8e
Yannick <ymetz@hotmail.de>
2022-05-17 13:18:31 +0200

    options hinzugefügt

--- tests_selenium/test_01_register.py
+++ tests_selenium/test_01_register.py
@@ -21,7 +21,7 @@ class register_test(unittest.TestCase, unittest.TextTestResult):
 
     def setUp(self):
         #driver für nachfolgende funktionen definieren
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     def test_register_customer(self):
         driver = self.driver
