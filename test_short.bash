553f6e5cdbf33f7cc65304b299ed88927f3bbac1
dominiksimgen <simgen.d@googlemail.com>
2022-07-10 17:52:08 +0200

    bugfixes

--- test_short.bash
+++ test_short.bash
@@ -1,2 +1,2 @@
 #! /bin/bash
-python3 -m pytest tests_pytest/app_pytest/ -v
+python3 -m pytest tests_pytest/app_pytest/
--- tests_pytest/app_pytest/test_02_views.py
+++ tests_pytest/app_pytest/test_02_views.py
@@ -372,7 +372,7 @@ def test_create_quotation(test_client):
     assert "_user_id" not in session
     login(test_client, email="erika@testmail.com", password="123456")
     assert session["_user_id"] == "2"
-    response = test_client.post('/quote/1', data=dict(quote=999.95123, service_start='9999-01-31', service_finish='9999-02-31'), follow_redirects=True)
+    response = test_client.post('/quote/1', data=dict(quote=999.95123, service_start='9999-01-31', service_finish='9999-02-25'), follow_redirects=True)
     assert response.status_code == 200
     assert response.request.path == '/order-details/1'
     assert b'999.95' in response.data
